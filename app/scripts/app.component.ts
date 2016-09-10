7fa3a779187909d6ea5aa33fd0dc01b4b0492c0f
antjanus <antonin@antjanus.com>
2016-09-09 23:21:18 -0600

    Grid start

--- app/scripts/app.component.ts
+++ app/scripts/app.component.ts
@@ -2,6 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: '<h1>My first Angular 2 App</h1>'
+  template: `
+    <div class="flex-container">
+      <div class="sidebar">
+        <h3>Files</h3>
+      </div>
+      <div class="flex-col">
+        <h2>Title</h2>
+      </div>
+    </div>
+  `
 })
 export class AppComponent { }
